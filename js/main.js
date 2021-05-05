38a3fed582004cf39fdfa385b4da51c643d9e6d8
Aleksandar <aristic665@gmail.com>
2021-05-06 00:42:08 +0200

    finished base layout

--- js/main.js
+++ js/main.js
@@ -3,15 +3,16 @@ let timeline = new TimelineMax();
 
 timeline
   .to('.fence', 3, { y: -300 })
-  .to('.grass', 3, { y: -200 }, '-=3')
+  .to('.grass', 3, { y: -350 }, '-=3')
   .to('.treeline', 3, { y: -200}, '-=3')
-  .fromTo('.main', 3, { y: 50 }, '-=3')
-  .to('.content', 3, { top:"0" }, '-=3');
+  .fromTo('.main', { y: -50 }, { y: 0, duration: 3 }, '-=3')
+  .to('.content', 3, { top:"0" }, '-=3')
+  .fromTo('.content-images', { opacity: 0 }, { opacity: 1, duration: 3 }, '-=3');
 
 let scene = new ScrollMagic.Scene({
   triggerElement: "section",
   triggerHook: 0,
-  duration: "100%"
+  duration: "200%"
 })
   .setTween(timeline)
   .setPin("section")
